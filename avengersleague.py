74ef839c5a3917e9c74ab947741c2b2e4c6d17eb
REICHKANZLER <91466006+REICHKANZLER@users.noreply.github.com>
2022-01-28 10:43:41 +0100

    Create avengersleague.py

---
+++ avengersleague.py
@@ -0,0 +1,44 @@
+
+my_list = ['Flash', 'Spiderman', 'Ironman', 'Hulken', 'Thor']
+
+# first item
+print(my_list[0])  
+
+# first item
+print(my_list[1])
+# first item
+print(my_list[2])
+
+# third item
+print(my_list[3])  
+
+# fifth item
+print(my_list[4])  
+
+
+my_list = ['Hawkeye', 'Aquaman', 'Flash', 'Deadpool', 'Blackpanther']
+
+# first item
+print(my_list[0])  
+
+# first item
+print(my_list[1])
+# first item
+print(my_list[2])
+
+# third item
+print(my_list[3])  
+
+# fifth item
+print(my_list[4])  
+
+
+import random
+
+secure_random = random.SystemRandom()
+print(secure_random.choice(my_list))
+
+import random
+
+secure_random = random.SystemRandom()
+print(secure_random.choice(my_list))
